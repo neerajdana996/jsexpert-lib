0469be471bba6126154bc5d9fd84f9830acfa225
bhavanad05 <bhavana.nd05@gmail.com>
2023-06-14 16:05:13 +0530

    changes for dashboad screen

--- src/Profilers/mongooseProfiler.ts
+++ src/Profilers/mongooseProfiler.ts
@@ -2,15 +2,15 @@
 
 import { DbPerformanceObject } from "../Models/DbPerformanceModel";
 
-const MongoPerformance = function (schema) {
+const MongoPerformance = function (schema:any) {
   // Intercept find method calls
-  schema.pre('find', function (next) {
+  schema.pre('find', function (next:any) {
     // Store the start time of the query
     this._queryStartTime = process.hrtime();
     next();
   });
 
-  schema.post('find', function (docs, next) {
+  schema.post('find', function (_:any, next:any) {
     // Calculate the query execution time
     const queryEndTime = process.hrtime(this._queryStartTime);
     const queryTimeInMs = (queryEndTime[0] * 1000) + (queryEndTime[1] / 1e6);
