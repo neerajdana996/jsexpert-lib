931803feca8ea6d9a91fe3d8e6370019fdbde22c
bhavanad05 <bhavana.nd05@gmail.com>
2023-06-14 16:04:26 +0530

    changes for dashboad screen

--- src/Profilers/mongooseProfiler.ts
+++ src/Profilers/mongooseProfiler.ts
@@ -1,45 +1,36 @@
-import { RequestObjectMapper } from "../Mappers/ReqToRequestObject";
+
 
 import { DbPerformanceObject } from "../Models/DbPerformanceModel";
 
-function MongoPerformance(options: any) {
-  return function (schema: any) {
-    // Intercept find method calls
-    schema.pre('find', function (next: any) {
-      // Store the start time of the query
-      this._queryStartTime = process.hrtime();
-      next();
-    });
+const MongoPerformance = function (schema) {
+  // Intercept find method calls
+  schema.pre('find', function (next) {
+    // Store the start time of the query
+    this._queryStartTime = process.hrtime();
+    next();
+  });
 
-    schema.post('find', function (_: any, next: any) {
-      // Calculate the query execution time
-      const queryEndTime = process.hrtime(this._queryStartTime);
-      const queryTimeInMs = (queryEndTime[0] * 1000) + (queryEndTime[1] / 1e6);
-      const collectionName = this.model.collection.name;
-      // Access the `req` object from options
-      const req = options.req;
-      if (req) {
-        const performanceObject: DbPerformanceObject = {
-          requestObject: RequestObjectMapper(req),
-          durationInMilliseconds: queryTimeInMs,
-          query: JSON.stringify(this.getQuery()),
-          collectionName
-        }
-        console.log('performanceObject', performanceObject);
-      } else {
-        const performanceObject: DbPerformanceObject = {
-          requestObject: null,
-          durationInMilliseconds: queryTimeInMs,
-          query: JSON.stringify(this.getQuery()),
-          collectionName
-        }
-        console.log('performanceObject', performanceObject);
+  schema.post('find', function (docs, next) {
+    // Calculate the query execution time
+    const queryEndTime = process.hrtime(this._queryStartTime);
+    const queryTimeInMs = (queryEndTime[0] * 1000) + (queryEndTime[1] / 1e6);
 
-      }
+    // Get the API path from the Mongoose model's collection name
+    const collectionName = this.model.collection.name;
+    const apiPath = collectionName.slice(0, collectionName.lastIndexOf('s'));
+    const performanceObject:DbPerformanceObject = {
+      durationInMilliseconds:queryTimeInMs,
+      query:JSON.stringify(this.getQuery()),
+      collectionName:collectionName
 
-      next();
-    });
-  };
+    }
+    // Log the query metrics with API path
+    console.log(`API Path: /${apiPath}`);
+    console.log(`MongoDB Query: ${collectionName}`);
+    console.log(`Query Execution Time: ${queryTimeInMs}ms`);
+    console.log(`performanceObject DB`,performanceObject);
+
+    next();
+  });
 };
-
 export default MongoPerformance;
