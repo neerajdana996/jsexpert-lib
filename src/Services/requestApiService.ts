cebfe363d6a5eca6f93271c50839bfac8478b160
bhavanad05 <bhavana.nd05@gmail.com>
2023-06-20 13:28:50 +0530

    new build

--- src/Services/requestApiService.ts
+++ src/Services/requestApiService.ts
@@ -1,85 +1,32 @@
 
-
-
-const http = require('http');
-
-const hostname = 'api.jsexpert.io';
-const apiCall = async (data: any, clientId: string, clientSecret: string,path:string) => {
-  const payload = JSON.stringify({ data });
-
-  const options = {
-    hostname,
-    path,
-    method: 'POST',
-
-
-    headers: {
-      'Content-Type': 'application/json',
-      clientId,
-      clientSecret
-    }
-  };
-
-  return new Promise((resolve, reject) => {
-    const req = http.request(options, (res: { on: (arg0: string, arg1: { (chunk: any): void; (): void; }) => void; statusCode: number; }) => {
-      let responseData = '';
-
-      res.on('data', (chunk:any) => {
-        responseData += chunk;
-      });
-
-      res.on('end', () => {
-        if (res.statusCode >= 200 && res.statusCode <= 299) {
-          resolve(JSON.parse(responseData));
-        } else {
-          reject(new Error(`Request failed with status code ${res.statusCode}`));
-        }
-      });
-    });
-
-    req.on('error', (error: any) => {
-      reject(error);
-    });
-
-    req.write(payload);
-    req.end();
-  });
-};
-
-
+import axios from 'axios';
 export const saveJsPerformance = async (data: any,clientId:any,clientSecret:any) => {
-    const response = await apiCall(data, clientId, clientSecret,'/server-data/');
- 
-    return response;
+
+
+    // const response = await fetch('https://api.jsexpert.io/server-data/', {
+    //     method: 'POST',
+    //     body: JSON.stringify({data}),
+    //     headers: {
+    //     'Content-Type': 'application/json',
+    //     clientId,
+    //     clientSecret
+    //     }
+    // })
+    const axiosres = await axios.post('https://api.jsexpert.io/server-data/', {data}, {
+        headers: {
+            clientId,
+            clientSecret
+        }
+    });
+    return axiosres.data;
 }
 
 export const saveMongoosePerformance = async (data: any,clientId:any,clientSecret:any) => {
-    const response = await apiCall(data, clientId, clientSecret,'/db-data/');
- 
-    return response;
+    const axiosres = await axios.post('https://api.jsexpert.io/db-data/', {data}, {
+        headers: {
+            clientId,
+            clientSecret
+        }
+    });
+    return axiosres.data;
 }
-// export const saveJsPerformance = async (data: any,clientId:any,clientSecret:any) => {
-//     const response = await fetch('https://api.jsexpert.io/server-data/', {
-//         method: 'POST',
-//         body: JSON.stringify({data}),
-//         headers: {
-//         'Content-Type': 'application/json',
-//         clientId,
-//         clientSecret
-//         }
-//     })
-//     return response.json();
-// }
-
-// export const saveMongoosePerformance = async (data: any,clientId:any,clientSecret:any) => {
-//     const response = await fetch('https://api.jsexpert.io/db-data/', {
-//         method: 'POST',
-//         body: JSON.stringify({data}),
-//         headers: {
-//         'Content-Type': 'application/json',
-//         clientId,
-//         clientSecret
-//         }
-//     })
-//     return response.json();
-// }
