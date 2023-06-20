372834c77928fa9655a20986db343a6a7b25889f
bhavanad05 <bhavana.nd05@gmail.com>
2023-06-20 13:58:25 +0530

    new build

--- src/Services/requestApiService.ts
+++ src/Services/requestApiService.ts
@@ -1,32 +1,26 @@
 
-import axios from 'axios';
 export const saveJsPerformance = async (data: any,clientId:any,clientSecret:any) => {
-
-
-    // const response = await fetch('https://api.jsexpert.io/server-data/', {
-    //     method: 'POST',
-    //     body: JSON.stringify({data}),
-    //     headers: {
-    //     'Content-Type': 'application/json',
-    //     clientId,
-    //     clientSecret
-    //     }
-    // })
-    const axiosres = await axios.post('https://api.jsexpert.io/server-data/', {data}, {
+    const response = await fetch('https://api.jsexpert.io/server-data/', {
+        method: 'POST',
+        body: JSON.stringify({data}),
         headers: {
-            clientId,
-            clientSecret
+        'Content-Type': 'application/json',
+        clientId,
+        clientSecret
         }
-    });
-    return axiosres.data;
+    })
+    return response.json();
 }
 
 export const saveMongoosePerformance = async (data: any,clientId:any,clientSecret:any) => {
-    const axiosres = await axios.post('https://api.jsexpert.io/db-data/', {data}, {
+    const response = await fetch('https://api.jsexpert.io/db-data/', {
+        method: 'POST',
+        body: JSON.stringify({data}),
         headers: {
-            clientId,
-            clientSecret
+        'Content-Type': 'application/json',
+        clientId,
+        clientSecret
         }
-    });
-    return axiosres.data;
+    })
+    return response.json();
 }
