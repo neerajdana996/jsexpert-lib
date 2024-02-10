9212ca863fdf522e5082ccb61f3db901c53957ed
bhavanad05 <bhavana.nd05@gmail.com>
2024-02-10 13:32:41 +0530

    new build

---
+++ test.js
@@ -0,0 +1,10 @@
+// write a react component for login screen 
+const Login = () => {
+    return (
+        <div>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button>login</button>
+        </div>
+    )
+}
