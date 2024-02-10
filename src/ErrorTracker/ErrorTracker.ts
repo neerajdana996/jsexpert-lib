24157364bc629dab9d81d5c51071225f06437f69
bhavanad05 <bhavana.nd05@gmail.com>
2024-02-10 14:01:20 +0530

    new build

--- src/ErrorTracker/ErrorTracker.ts
+++ src/ErrorTracker/ErrorTracker.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 
 export class ErrorTracker {
   private sourceCodeCache: Map<string, string[]>;
@@ -8,7 +8,7 @@ export class ErrorTracker {
   }
 
   captureError(error: Error): void {
-    const stackTrace:any = error.stack;
+    const stackTrace: any = error.stack;
     const errorLines = stackTrace.split('\n');
 
     const sourceCode = this.retrieveSourceCode(errorLines);
@@ -19,46 +19,46 @@ export class ErrorTracker {
     const sourceCodeLines: any[] = [];
     const matches = errorLines[1].match(/at (.+):(\d+):(\d+)/);
     if (matches) {
-        console.log('matches', matches);
+      console.log('matches', matches);
 
-        //cons match 1 = `at /home/neeraj/works/jsexper/jslib/node-express-typescript/dist/index.js:46:15`
-        const filePath = matches[1];
-        const lineNumber = parseInt(matches[2]);
-          const sourceCodeLine = this.getSourceCodeLine(filePath, lineNumber);
-      
+      //cons match 1 = `at /home/neeraj/works/jsexper/jslib/node-express-typescript/dist/index.js:46:15`
+      const filePath = matches[1];
+      const lineNumber = parseInt(matches[2]);
+      const sourceCodeLine = this.getSourceCodeLine(filePath, lineNumber);
+
       if (sourceCodeLine) {
-      
+
         sourceCodeLines.push({
-            filePath, lineNumber,sourceCodeLine
+          filePath, lineNumber, sourceCodeLine
         });
       }
     }
-  
-    return sourceCodeLines.map(a=>JSON.stringify(a.sourceCodeLine,null,4)).join('\n');
+
+    return sourceCodeLines.map(a => JSON.stringify(a.sourceCodeLine, null, 4)).join('\n');
   }
-  
+
   getSourceCode(filePath: string, fromLine: number, toLine: number): string {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const fileContent = readFileSync(filePath, 'utf8');
     const lines = fileContent.split('\n');
-  
+
     return lines.slice(fromLine, toLine).join('\n');
   }
   getSourceCodeLine(filePath: string, lineNumber: number) {
 
-    const fromLine = lineNumber - 5 > 0 ? lineNumber - 5 : 0 ;
+    const fromLine = lineNumber - 5 > 0 ? lineNumber - 5 : 0;
     const toLine = lineNumber + 5;
 
     console.log('filePath', filePath);
- 
-   
+
+
     if (this.sourceCodeCache.has(filePath)) {
       return this.sourceCodeCache.get(filePath)![lineNumber - 1];
     }
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const fileContent = readFileSync(filePath, 'utf8');
     const allLines = fileContent.split('\n');
     const lines = allLines.slice(fromLine, toLine)
-    console.log('lineNumber', lineNumber,fromLine,toLine,allLines.length,lines.length);
-  
+    console.log('lineNumber', lineNumber, fromLine, toLine, allLines.length, lines.length);
+
     this.sourceCodeCache.set(filePath, lines);
     return lines;
   }
